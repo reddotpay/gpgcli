77af3a49f476ff92a8dbbecb1f3db93991c17b26
Gian Carlo Val Flores Ebao <gianebao@reddotpay.com>
2019-07-12 10:24:05 +0800

    initial

---
+++ README.md
@@ -0,0 +1,61 @@
+# GPGCLI
+
+## Generating GPG keys
+
+1. List all available keys
+
+    ```
+    gpg --list-keys
+    ```
+
+2. Create a new key
+    The adviced configuration is:
+
+    | Question    | Answer                                |
+    | ---         | ---                                   |
+    | kind of key | (1) RSA and RSA (default)             |
+    | keysize     | 4096                                  |
+    | *validity   | 0 = key does not expire (for staging) |
+
+    *validity should be specified for production.
+
+    ```
+    gpg --full-generate-key
+    ```
+
+3. Export the keys
+
+    ```
+    gpg --armor --output <uid>.gpg.pub --export <uid>
+    gpg --armor --output <uid>.gpg.pvt --export-secret-keys <uid>
+    ```
+
+## Usage
+
+To encrypt a file,
+
+```
+gpgcli encrypt <file> --public <public-file-gpg> --output <outfile>
+```
+
+Omitting the `--output` will dump the result in base64
+
+For when decrypting,
+
+```
+gpgcli encrypt <file> --public <public-file-gpg> --output <outfile>
+```
+
+## Troubleshooting
+| #   | Error                                                   |                                                                                |
+| --- | ---                                                     | ---                                                                            |
+|  1  | `missing command`                                       | use either `help`, `encrypt`, or `decrypt`                                       |
+|  2  | ``command `<command>` not supported``                   | similar to 1                                                                   |
+|  3  | `missing file`                                          | missing file to be encrypted or decrypted                                      |
+|  4  | ``missing `--public` argument while using encrypt``     | `encrypt` command should contain a `--public` which points the public GPG file |
+|  5  | ``missing `--secret` argument while using decrypt``     | `decrypt` command should contain a `--secret` which points the secret GPG file |
+|  6  | ``missing `--passphrase` argument while using decrypt`` | `decrypt` command should contain a `--passphrose` with the secret passphrase   |
+
+## .onsave
+
+`.onsave` is a plugin used by Visual Studio Code which executes when files are changed. Inside the `.onsave` file is the linux command on how to compile the application in multiple OS' and stored in the `dist` folder.
---
+++ main.go
@@ -0,0 +1,192 @@
+package main
+
+import (
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+
+	"github.com/reddotpay/gogpg"
+)
+
+const HELP = `
+encrypt or decrypt using gnu/pg or gpg
+
+gpgcli help
+gpgcli encrypt <file> --public <public-file-gpg> [--output <outfile>]
+gpgcli decrypt <file> --secret <secret-file-gpg> --passphrase <passphrase> [--output <outfile>]
+
+Commands:
+encrypt - encrypts the file
+decrypt - decrypts the file
+help    - displays this help
+
+Options:
+--public     - gpg public key
+--secret     - gpg secret key
+--passphrase - gpg secret passphrase
+--output     - dumps the output to this file
+`
+
+func main() {
+	var (
+		command string
+		err     error
+		buf     []byte
+
+		args = parseArgs()
+
+		output     = flagAssign(args, "--output", "")
+		public     = flagAssign(args, "--public", "")
+		secret     = flagAssign(args, "--secret", "")
+		passphrase = flagAssign(args, "--passphrase", "")
+	)
+
+	command, err = getCommand()
+	handleErr(err)
+
+	switch command {
+	case "-h":
+		fmt.Print(HELP)
+		return
+
+	case "encrypt":
+		if buf, err = encrypt(public); nil != err {
+			throwErr(err.Error())
+		}
+		break
+
+	case "decrypt":
+		if buf, err = decrypt(secret, passphrase); nil != err {
+			throwErr(err.Error())
+		}
+		break
+
+	default:
+		throwErr(fmt.Sprintf("command `%s` not supported", command))
+	}
+
+	if "" != output {
+		ioutil.WriteFile(output, buf, 0644)
+	} else {
+		fmt.Print(base64.StdEncoding.EncodeToString(buf))
+	}
+}
+
+// parseArgs parses the arguments and maps with the -- parameter
+func parseArgs() map[string]string {
+	var (
+		flagPattern = regexp.MustCompile(`^\-\-[\-a-z0-9]+$`)
+		params      = map[string]string{}
+		activeFlag  = ""
+	)
+
+	for _, f := range os.Args {
+		if "" != activeFlag {
+			params[activeFlag] = f
+			activeFlag = ""
+			continue
+		}
+
+		if flagPattern.MatchString(f) {
+			activeFlag = f
+			continue
+		}
+	}
+
+	return params
+}
+
+// flagAssign searches the flag map and assigns the default value what the key is not found
+func flagAssign(flag map[string]string, key, defaultVal string) string {
+	if _, ok := flag[key]; ok {
+		return flag[key]
+	}
+
+	return defaultVal
+}
+
+// throwErr displays the [m]essage and kills the program
+func throwErr(m string) {
+	fmt.Printf("error: %s. use help for help\n", m)
+	os.Exit(1)
+}
+
+// handleErr validates an error and uses `throwErr`
+func handleErr(err error) {
+	if nil == err {
+		return
+	}
+
+	throwErr(err.Error())
+}
+
+// getCommand fetches the first argument from the CLI
+func getCommand() (string, error) {
+	if 1 >= len(os.Args) {
+		return "", errors.New("missing command")
+	}
+
+	return os.Args[1], nil
+}
+
+// loadTargetFile checks the second argument and assumes it's the target file
+func loadTargetFile() ([]byte, error) {
+	if 2 >= len(os.Args) {
+		return nil, errors.New("missing file")
+	}
+
+	return ioutil.ReadFile(os.Args[2])
+}
+
+// encrypt encrypts the file with public key
+func encrypt(key string) ([]byte, error) {
+	var (
+		f   []byte
+		kf  *os.File
+		err error
+	)
+
+	if f, err = loadTargetFile(); nil != err {
+		return nil, err
+	}
+
+	if "" == key {
+		return nil, errors.New("missing `--public` argument while using encrypt")
+	}
+
+	if kf, err = os.Open(key); nil != err {
+		return nil, err
+	}
+
+	return gogpg.Encrypt(kf, f)
+}
+
+// decrypt decrypts the file with private key and passphrase
+func decrypt(key, passphrase string) ([]byte, error) {
+	var (
+		f   []byte
+		kf  *os.File
+		err error
+	)
+
+	if f, err = loadTargetFile(); nil != err {
+		return nil, err
+	}
+
+	if "" == key {
+		return nil, errors.New("missing `--secret` argument while using decrypt")
+	}
+
+	if "" == passphrase {
+		return nil, errors.New("missing `--passphrase` argument while using decrypt")
+	}
+
+	if kf, err = os.Open(key); nil != err {
+		return nil, err
+	}
+
+	return gogpg.Decrypt(kf, passphrase, f)
+}
